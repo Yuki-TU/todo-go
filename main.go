9c0a9c35b8bc7eddb0682ab5028f2bc593d02210
Yuki-TU <k.yuki.90219@icloud.com>
2022-08-26 01:33:32 +0900

    add:#1 corsの設定

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 
 	"github.com/Yuki-TU/todo-go/routers"
 	"github.com/Yuki-TU/todo-go/utils"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		// TODO: 一旦全てのドメインを許可
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"*"},
+		AllowHeaders:  []string{"*"},
+		AllowWildcard: true,
+	}))
 	routers.SetRouting(router)
 	log.Fatal(router.Run(":" + utils.GodotEnv("GO_PORT")))
 }
