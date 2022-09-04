902cab0cdbad097becf9583ae4f13addeaa82623
Yuki-TU <k.yuki.90219@icloud.com>
2022-09-04 23:17:17 +0900

    add:#3 登録処理 * service, repository, handlersに分けて記述

---
+++ controllers/user/register/service.go
@@ -0,0 +1,35 @@
+package registerAuth
+
+import (
+	model "github.com/Yuki-TU/todo-go/models"
+)
+
+type Service interface {
+	RegisterService(input *InputRegister) (*model.EntityUsers, string)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewServiceRegister(repository Repository) *service {
+	return &service{repository: repository}
+}
+
+// ハンドラーとリポジトリをつなげる
+// @params input POSTデータ
+//
+// @return resultRegister 登録結果
+//
+// @return errRegister 登録の際発生したエラー
+func (s *service) RegisterService(input *InputRegister) (*model.EntityUsers, string) {
+	// 入力データをきれいにして登録処理を行う
+	users := model.EntityUsers{
+		Fullname: input.Fullname,
+		Email:    input.Email,
+		Password: input.Password,
+	}
+
+	resultRegister, errRegister := s.repository.RegisterRepository(&users)
+	return resultRegister, errRegister
+}
