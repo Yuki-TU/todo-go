e4910444a10da567381205d47a396b9119b94088
Yuki-TU <k.yuki.90219@icloud.com>
2022-09-04 23:44:41 +0900

    add:#3 routerの設定

--- routers/router.go
+++ routers/router.go
@@ -1,24 +1,26 @@
 package routers
 
 import (
+	"database/sql"
+
+	registerAuth "github.com/Yuki-TU/todo-go/controllers/user/register"
+	handlerRegister "github.com/Yuki-TU/todo-go/handlers/users"
 	"github.com/gin-gonic/gin"
 )
 
 // ルーティングの設定を行う
 //
 // @param router ルーター
-func SetRouting(router *gin.Engine) {
-	router.GET("/", func(c *gin.Context) {
+func SetRouting(db *sql.DB, router *gin.Engine) {
+	registerRepository := registerAuth.NewRepositoryRegister(db)
+	registerService := registerAuth.NewServiceRegister(registerRepository)
+	registerHandler := handlerRegister.NewHandlerRegister(registerService)
+
+	groupRoute := router.Group("/api/v1")
+	groupRoute.GET("", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello World",
 		})
 	})
-
-	router.POST("/users", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"email":    "hoge@hoge.com",
-			"fullname": "GET",
-			"passwrod": "成功しました。",
-		})
-	})
+	groupRoute.POST("/users", registerHandler.RegisterHandler)
 }
